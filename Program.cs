8d7c14224722ea0ca20a66a7f7c8a41e2d852c70
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-13 11:17:48 +0700

    Menambahkan Class Laptop

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ class KodeProduk {
         return KodeProdukMap.TryGetValue(produk, out string KodeProduk) ? KodeProduk : "Kode produk tidak ditemukan";
     }
 }
-
+/*
 class Program {
     static void Main()
     {
@@ -41,4 +41,46 @@ class Program {
         string input = Console.ReadLine();
         Console.WriteLine($"Kode pos {input}: {KodeProduk.GetKodeProduk(input)}");
     }
+}*/
+
+class FanLaptop {
+    public enum State {Quiet, Balanced, Performance, Turbo};
+    public enum Trigger {ModeUp, ModeDown, Turboshortcut};
+    private State currentState;
+    class changeMode
+    {
+        public State stateAwal;
+        public Trigger trigger;
+        public State stateAkhir;
+        public changeMode(State stateAwal, Trigger trigger, State stateAkhir)
+        {
+            this.stateAwal = stateAwal;
+            this.trigger = trigger;
+            this.stateAkhir = stateAkhir;
+        }
+    }
+        private changeMode[] changes =
+        {
+            new changeMode(State.Quiet, Trigger.ModeUp, State.Balanced),
+            new changeMode(State.Balanced, Trigger.ModeUp, State.Performance),
+            new changeMode(State.Performance, Trigger.ModeUp, State.Turbo),
+            new changeMode(State.Turbo, Trigger.ModeDown, State.Performance),
+            new changeMode(State.Performance, Trigger.ModeDown, State.Balanced),
+            new changeMode(State.Balanced, Trigger.ModeDown, State.Quiet),
+            new changeMode(State.Quiet, Trigger.Turboshortcut, State.Turbo),
+            new changeMode(State.Balanced, Trigger.Turboshortcut, State.Turbo)
+        };
+    public String GetStaticState(State stateAwal, Trigger trigger)
+    {
+        for (int i = 0; i < changes.Length; i++) {
+            if (stateAwal == changes[i].stateAwal && changes[i].trigger == trigger) {
+                return "Fan" + stateAwal+ " Berubah menjadi" + changes[i];
+            } 
+        }
+        return "Fan tetap" + stateAwal;
+    }
+}
+
+class Program { 
+    
 }
